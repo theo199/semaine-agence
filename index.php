3df81a9581d5c5621fa97f34f31e09d876721bea
theo199 <thetke2@yahoo.fr>
2020-02-04 13:39:58 +0100

    commit1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Exemple - Mot de passe</title>
+	</head>
+	<body>
+	<h2>Enregistrement</h2>
+	<form action="register.php" method="POST">
+		<label>Identifiant :</label>
+		<input type="text" name="username" required /><br /><br />
+		<label>Mot de passe :</label>
+		<input type="password" name="password" required /><br /><br />
+		<label>Retapez mot de passe :</label>
+		<input type="password" name="password2" required /><br /><br />
+		<input type="submit" />
+	</form>
+	<br /><hr />
+	<h2>Connexion</h2>
+	<form action="login.php" method="POST">
+		<label>Identifiant :</label>
+		<input type="text" name="username" required /><br /><br />
+		<label>Mot de passe :</label>
+		<input type="password" name="password" required /><br /><br />
+		<input type="submit" />
+	</form>
+	</body>
+</html>
